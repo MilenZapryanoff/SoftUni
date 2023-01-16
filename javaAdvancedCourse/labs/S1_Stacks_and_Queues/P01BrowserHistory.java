39f0f2b60bbea08c13983e58904dff39ec8dbe43
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-01-16 16:16:15 +0200

    Create P01BrowserHistory.java

---
+++ javaAdvancedCourse/labs/S1_Stacks_and_Queues/P01BrowserHistory.java
@@ -0,0 +1,28 @@
+package S1_Stacks_and_Queues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P01BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        while (!input.equals("Home")) {
+            if (input.equals("back")) {
+                if (stack.size() > 1) {
+                    stack.pop();
+                    System.out.println(stack.peek());
+                } else {
+                    System.out.println("no previous URLs");
+                }
+            } else {
+                stack.push(input);
+                System.out.println(stack.peek());
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
