e0e5c26fc2e4eb9fd858a18c7ae548803ae1dc0c
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-01-16 16:07:47 +0200

    Create P04MatchingBrackets.java

---
+++ javaAdvancedCourse/labs/S1_Stacks_and_Queues/P04MatchingBrackets.java
@@ -0,0 +1,27 @@
+package S1_Stacks_and_Queues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
+public class P04MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        ArrayDeque<Integer> indexes = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                indexes.push(i);
+            } else if (input.charAt(i) == ')') {
+                int openBracketIndex = indexes.pop();
+
+                String expression = input.substring(openBracketIndex, i + 1);
+                System.out.println(expression);
+
+            }
+        }
+    }
+}
