aade1ee52de1f5bc091a0703000e0250b2a51f93
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-01-16 17:07:46 +0200

    Create P05PrinterQueue.java

---
+++ javaAdvancedCourse/labs/S1_Stacks_and_Queues/P05PrinterQueue.java
@@ -0,0 +1,30 @@
+package S1_Stacks_and_Queues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P05PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> queue = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("print")) {
+            if (!input.equals("cancel")) {
+                queue.offer(input);
+            } else {
+                if (queue.size() > 0) {
+                    String printToBeCancelled = queue.poll();
+                    System.out.println("Canceled " + printToBeCancelled);
+                } else {
+                    System.out.println("Printer is on standby");
+                }
+            }
+
+            input = scanner.nextLine();
+        }
+        queue.forEach(System.out::println);
+    }
+}
