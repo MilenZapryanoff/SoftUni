17dd132b0360100cda87344eefec00fe7061a29c
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-01-17 10:38:53 +0200

    Update P02BasicStackOperations.java

--- javaAdvancedCourse/exercises/S1_Stacks_and_Queues/P02BasicStackOperations.java
+++ javaAdvancedCourse/exercises/S1_Stacks_and_Queues/P02BasicStackOperations.java
@@ -6,17 +6,15 @@ public class P02BasicStackOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] commandArr = scanner.nextLine().split("\\s+");
-        String[] digitsArr = scanner.nextLine().split("\\s+");
+        int digitsToPush = scanner.nextInt();
+        int digitsToPop = scanner.nextInt();
+        int digitToCheck = scanner.nextInt();
 
-        int digitsToPush = Integer.parseInt(commandArr[0]);
-        int digitsToPop = Integer.parseInt(commandArr[1]);
-        String digitToCheck = commandArr[2];
-
-        ArrayDeque<String> stack = new ArrayDeque<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
         for (int i = 0; i < digitsToPush; i++) {
-            String currentDigitToPop = digitsArr[i];
+
+            Integer currentDigitToPop = scanner.nextInt();
             stack.push(currentDigitToPop);
         }
         for (int i = 0; i < digitsToPop; i++) {
@@ -28,8 +26,8 @@ public class P02BasicStackOperations {
                 System.out.println("true");
             } else {
                 int smallestNum = Integer.MAX_VALUE;
-                for (String element : stack) {
-                    int currentNum = Integer.parseInt(element);
+                for (Integer element : stack) {
+                    int currentNum = element;
                     if (currentNum < smallestNum) {
                         smallestNum = currentNum;
                     }
