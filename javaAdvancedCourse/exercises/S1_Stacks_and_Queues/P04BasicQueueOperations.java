9557baff43fb86670d6b9a848e529cd2c795d0cb
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-01-17 11:02:57 +0200

    Create P04BasicQueueOperations.java

---
+++ javaAdvancedCourse/exercises/S1_Stacks_and_Queues/P04BasicQueueOperations.java
@@ -0,0 +1,44 @@
+package S1_Stacks_and_Queues;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class P04BasicQueueOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int digitsToPush = scanner.nextInt();
+        int digitsToPop = scanner.nextInt();
+        int digitToCheck = scanner.nextInt();
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < digitsToPush; i++) {
+
+            Integer currentDigitToPop = scanner.nextInt();
+            stack.offer(currentDigitToPop);
+        }
+        for (int i = 0; i < digitsToPop; i++) {
+            stack.poll();
+        }
+
+        if (!stack.isEmpty()) {
+            if (stack.contains(digitToCheck)) {
+                System.out.println("true");
+            } else {
+//                int smallestNum = Integer.MAX_VALUE;
+//                for (Integer element : stack) {
+//                    int currentNum = element;
+//                    if (currentNum < smallestNum) {
+//                        smallestNum = currentNum;
+//                    }
+//                }
+//                System.out.println(smallestNum);
+                System.out.println(Collections.min(stack));
+            }
+        } else {
+            System.out.println(0);
+        }
+    }
+}
