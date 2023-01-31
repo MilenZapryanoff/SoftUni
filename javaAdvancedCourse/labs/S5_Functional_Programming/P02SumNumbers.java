8b1062f01c0ca80afa629a33996ab79677641dc4
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-01-31 17:09:19 +0200

    Create P02SumNumbers.java

---
+++ javaAdvancedCourse/labs/S5_Functional_Programming/P02SumNumbers.java
@@ -0,0 +1,28 @@
+package S5_Functional_Programming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class P02SumNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Function<String, Integer> parse = s -> Integer.parseInt(s);
+
+        List<Integer> numberList = Arrays.stream(scanner.nextLine().split(", "))
+                .map(elem -> parse.apply(elem))
+                .collect(Collectors.toList());
+
+        int sum = 0;
+        for (int i = 0; i < numberList.size(); i++) {
+            int currentNum = numberList.get(i);
+            sum += currentNum;
+        }
+        System.out.printf("Count = %d%n", numberList.size());
+        System.out.printf("Sum = %d%n", sum);
+    }
+}
