41a226a254e967a90b8be0b38a7729ade4aa69c5
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-01 16:29:42 +0200

    Create P07FindTheSmallestElement.java

---
+++ javaAdvancedCourse/exercises/S5_Functional_Programming/P07FindTheSmallestElement.java
@@ -0,0 +1,22 @@
+package S5_Functional_Programming;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class P07FindTheSmallestElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        Function<List<Integer>, Integer> getLastIndexOfMinElement = list -> list.lastIndexOf(Collections.min(list));
+
+        System.out.println(getLastIndexOfMinElement.apply(numbers));
+    }
+}
