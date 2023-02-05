0f3aadb319e19884dbba009a4756586e9891cc8c
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-06 00:08:13 +0200

    Create Person.java

---
+++ javaAdvancedCourse/exercises/S6_Defining_classes/P01OpinionPoll/Person.java
@@ -0,0 +1,16 @@
+package S6_Defining_classes.P01OpinionPoll;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + age;
+    }
+}
