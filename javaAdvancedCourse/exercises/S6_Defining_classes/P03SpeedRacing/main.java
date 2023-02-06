a436860ec958f00baf347e355e810e79141f2ec8
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-07 00:14:51 +0200

    Create main.java

---
+++ javaAdvancedCourse/exercises/S6_Defining_classes/P03SpeedRacing/main.java
@@ -0,0 +1,46 @@
+package S6_Defining_classes.P03SpeedRacing;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        LinkedHashMap<String, Car> cars = new LinkedHashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] data = scanner.nextLine().split("\\s+");
+            String model = data[0];
+            int fuelAmount = Integer.parseInt(data[1]);;
+            double priceFuelPerKm = Double.parseDouble(data[2]);
+
+            Car car = new Car(model, fuelAmount, priceFuelPerKm);
+            cars.put(model, car);
+
+        }
+        String command = scanner.nextLine();
+
+        while (!command.equals("End")) {
+            String[] commandLine = command.split("\\s+");
+            String carModelToDrive = commandLine[1];
+            int kmToDrive = Integer.parseInt(commandLine[2]);
+
+            //вимам колата, която ще карам
+            Car carToDrive = cars.get(carModelToDrive);
+
+            if (!carToDrive.drive(kmToDrive)) {
+                System.out.println("Insufficient fuel for the drive");
+            }
+
+
+
+            command = scanner.nextLine();
+        }
+        for (Car car : cars.values()) {
+            System.out.println(car);
+        }
+    }
+}
