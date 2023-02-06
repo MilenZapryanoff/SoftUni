b269dcf2770f91ee418793aaa4f55a6312dbc1ed
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-07 00:14:36 +0200

    Create Car.java

---
+++ javaAdvancedCourse/exercises/S6_Defining_classes/P03SpeedRacing/Car.java
@@ -0,0 +1,30 @@
+package S6_Defining_classes.P03SpeedRacing;
+
+public class Car {
+    private String model;
+    private double fuelAmount;
+    private double priceFuelPerKm;
+    private int distanceTraveled;
+
+    public Car(String model, double fuelAmount, double priceFuelPerKm) {
+        this.model = model;
+        this.fuelAmount = fuelAmount;
+        this.priceFuelPerKm = priceFuelPerKm;
+        this.distanceTraveled = 0;
+    }
+
+    public boolean drive(int kmDrive) {
+        double needFuel = kmDrive * this.priceFuelPerKm;
+        if (needFuel <= this.fuelAmount) {
+            this.fuelAmount -= needFuel;
+            this.distanceTraveled += kmDrive;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %.2f %d", model, fuelAmount, distanceTraveled);
+    }
+}
