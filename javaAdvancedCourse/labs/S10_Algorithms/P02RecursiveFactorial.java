db122fc2a807976cd6f1cbb2a5afc690a247d6a8
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-16 23:10:35 +0200

    Create P02RecursiveFactorial.java

---
+++ javaAdvancedCourse/labs/S10_Algorithms/P02RecursiveFactorial.java
@@ -0,0 +1,21 @@
+package S10_Algorithms;
+
+import java.util.Scanner;
+
+public class P02RecursiveFactorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(factorial(n));
+    }
+
+    private static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+}
