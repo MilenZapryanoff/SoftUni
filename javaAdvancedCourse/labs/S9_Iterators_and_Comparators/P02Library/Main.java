3c8304dac3dd21aa1fc02184aa2db2e6d85f51c8
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-16 22:55:38 +0200

    Create Main.java

---
+++ javaAdvancedCourse/labs/S9_Iterators_and_Comparators/P02Library/Main.java
@@ -0,0 +1,30 @@
+package S9_Iterators_and_Comparators.P02Library;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+
+        Library lib = new Library(bookOne, bookTwo, bookThree);
+
+        for (Book book : lib) {
+            // for-body
+
+            System.out.println(book.getTitle());
+        }
+
+        lib.forEach(book -> System.out.println(book.getTitle()));
+
+        Iterator<Book> iterator = lib.iterator();
+        while (iterator.hasNext()) {
+            Book current = iterator.next(); // 0, 1, 2
+
+            // for-body
+
+            System.out.println(current.getTitle());
+        }
+    }
+}
