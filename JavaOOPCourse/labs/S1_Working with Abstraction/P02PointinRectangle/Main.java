320c5123b74746d1286bc6e7e9f89a796602eb0c
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-24 16:39:09 +0200

    Create Main.java

---
+++ JavaOOPCourse/labs/S1_Working with Abstraction/P02PointinRectangle/Main.java
@@ -0,0 +1,32 @@
+package S1_Working_with_Abstraction.P02PointinRectangle;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //"{bottomLeftX} {bottomLeftY} {topRightX} {topRightY}".
+
+        int[] coordinates = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int bottomLeftX = coordinates[0];
+        int bottomLeftY = coordinates[1];
+        int topRightX = coordinates[2];
+        int topRightY = coordinates[3];
+
+        Point bottomLeft = new Point(bottomLeftX, bottomLeftY);
+        Point topRight = new Point(topRightX, topRightY);
+        Rectangle rectangle = new Rectangle(bottomLeft, topRight);
+
+        int numberOfInputs = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < numberOfInputs; i++) {
+            int[] checkCoordinates = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            int x = checkCoordinates[0];
+            int y = checkCoordinates[1];
+            Point currentPoint = new Point(x, y);
+            System.out.println(rectangle.contains(currentPoint));
+        }
+    }
+}
