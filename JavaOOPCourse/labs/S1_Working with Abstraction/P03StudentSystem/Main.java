fa343556465da0dfdbbb486ac5abe699e75940c3
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-24 19:45:03 +0200

    Create Main.java

---
+++ JavaOOPCourse/labs/S1_Working with Abstraction/P03StudentSystem/Main.java
@@ -0,0 +1,22 @@
+package S1_Working_with_Abstraction.P03StudentSystem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StudentSystem studentSystem = new StudentSystem();
+
+        String[] input = readLine(scanner);
+
+        while (!input[0].equals("Exit")) {
+            studentSystem.parseCommand(input);
+
+            input = readLine(scanner);
+        }
+    }
+
+    private static String[] readLine(Scanner scanner) {
+        return scanner.nextLine().split("\\s+");
+    }
+}
