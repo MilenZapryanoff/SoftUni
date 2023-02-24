a9b4809dbc6789d3dcfcca88a3a6300f499738ae
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-24 16:40:07 +0200

    Create Point.java

---
+++ JavaOOPCourse/labs/S1_Working with Abstraction/P02PointinRectangle/Point.java
@@ -0,0 +1,27 @@
+package S1_Working_with_Abstraction.P02PointinRectangle;
+
+public class Point {
+    private int X;
+    private int Y;
+
+    public Point(int x, int y) {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+}
