478a24d0d71bb71867fa84c749cfa7d0be30055d
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-24 20:39:47 +0200

    Create DiscountType.java

---
+++ JavaOOPCourse/labs/S1_Working with Abstraction/P04HotelReservation/DiscountType.java
@@ -0,0 +1,31 @@
+package S1_Working_with_Abstraction.P04HotelReservation;
+
+public enum DiscountType {
+    NONE("None", 0),
+    SECONDVISIT("SecondVisit", 10),
+    VIP("VIP", 20);
+
+    private String name;
+    private double percent;
+
+    DiscountType(String name, double percent) {
+        this.name = name;
+        this.percent = percent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+
+    public void setPercent(double percent) {
+        this.percent = percent;
+    }
+}
