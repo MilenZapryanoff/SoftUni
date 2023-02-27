a47b77237816955a30c21d7320c129a7e0054095
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-02-27 12:59:02 +0200

    Update Card.java

--- JavaOOPCourse/exercises/S1_Working with Abstraction/P03CardsWithPower/Card.java
+++ JavaOOPCourse/exercises/S1_Working with Abstraction/P03CardsWithPower/Card.java
@@ -15,34 +15,25 @@ public class Card {
     }
 
     public int getPower() {
-        return this.rank.getPower() + this.suit.getPower();
+        return this.rank.power + this.suit.power;
     }
 
     enum Suit {
         CLUBS(0), DIAMONDS(13), HEARTS(26), SPADES(39);
 
-        int power;
-
+        public final int power;
         Suit(int power) {
             this.power = power;
         }
 
-        public int getPower() {
-            return this.power;
-        }
     }
 
     enum Rank {
         ACE(14), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13);
 
-        int power;
-
+        public final int power;
         Rank(int power) {
             this.power = power;
-        }
-
-        public int getPower() {
-            return this.power;
         }
     }
 }
