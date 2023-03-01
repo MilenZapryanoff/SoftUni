f31141bce18da8cd74e0b09ed22ff56fa4618761
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-02 00:02:06 +0200

    Create Main.java

---
+++ JavaOOPCourse/labs/S2_Encapsulation/P04FirstAndReserveTeam/Main.java
@@ -0,0 +1,34 @@
+package S2_Encapsulation.P04FirstAndReserveTeam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Person> players = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split("\\s+");
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            Person personToAdd = new Person(firstName, lastName, age, salary);
+            players.add(personToAdd);
+        }
+
+
+        Team team = new Team("Black Eagles");
+        for (Person player : players) {
+            team.addPlayer(player);
+        }
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getReserveTeam().size() + " players");
+    }
+}
