f68a15b401aca3abc21d98709292741bbb875c11
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-02 16:20:55 +0200

    Create Main.java

---
+++ JavaOOPCourse/exercises/S2_Encapsulation/P02AnimalFarm/Main.java
@@ -0,0 +1,16 @@
+package S2_Encapsulation.P02AnimalFarm;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        Chicken chicken = new Chicken(name, age);
+
+        System.out.printf("Chicken %s can produce %.2f eggs per day.", chicken.toString(), chicken.productPerDay() );
+    }
+}
