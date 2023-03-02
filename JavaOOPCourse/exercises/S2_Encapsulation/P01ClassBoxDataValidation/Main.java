aed9294927035e40e92026d61c14891deff0969e
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-02 15:26:53 +0200

    Create Main.java

---
+++ JavaOOPCourse/exercises/S2_Encapsulation/P01ClassBoxDataValidation/Main.java
@@ -0,0 +1,20 @@
+package S2_Encapsulation.P01ClassBoxDataValidation;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double length = Double.parseDouble(scanner.nextLine());
+        double width = Double.parseDouble(scanner.nextLine());
+        double height = Double.parseDouble(scanner.nextLine());
+
+        Box box = new Box(length, width, height);
+
+        System.out.printf("Surface Area - %.2f%n", box.calculateSurfaceArea());
+        System.out.printf("Lateral Surface Area - %.2f%n", box.calculateLateralSurfaceArea());
+        System.out.printf("Volume – %.2f%n2", box.calculateVolume());
+
+    }
+}
