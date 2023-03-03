e19ea93c51b95bf799855014bd4433a34334752e
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-03 02:15:00 +0200

    Create Product.java

---
+++ JavaOOPCourse/exercises/P03ShoppingSpree/Product.java
@@ -0,0 +1,35 @@
+package S2_Encapsulation.P03ShoppingSpree;
+
+public class Product {
+    private String name;
+    private double cost;
+
+    public Product(String name, double cost) {
+        setName(name);
+        setCost(cost);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        if (!name.trim().isEmpty()) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    private void setCost(double cost) {
+        if (cost >= 0) {
+            this.cost = cost;
+        } else {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+    }
+}
