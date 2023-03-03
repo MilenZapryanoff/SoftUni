fdb25b56ca8255b9af3f476c4d296d9be48db5f7
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-03 22:24:30 +0200

    Create Pizza.java

---
+++ JavaOOPCourse/exercises/S2_Encapsulation/P04PizzaCalories/Pizza.java
@@ -0,0 +1,49 @@
+package S2_Encapsulation.P04PizzaCalories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza {
+
+    private String name;
+    private Dough dough;
+    private List<Topping> toppings;
+
+
+    public Pizza(String name, int numberOfToppings) {
+        setName(name);
+        setToppings(numberOfToppings);
+    }
+
+    private void setToppings(int numberOfToppings) {
+        if (numberOfToppings >= 0 && numberOfToppings <= 10) {
+            this.toppings = new ArrayList<>(numberOfToppings);
+        } else {
+            throw new IllegalArgumentException("Number of toppings should be in range [0..10].");
+        }
+    }
+
+    public void setDough(Dough dough) {
+        this.dough = dough;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName(String name) {
+        if (name.trim().length() >= 1 && name.trim().length() <= 15) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Pizza name should be between 1 and 15 symbols.");
+        }
+    }
+
+    public void addTopping(Topping topping) {
+        this.toppings.add(topping);
+    }
+
+    public double getOverallCalories() {
+        return dough.calculateCalories() + toppings.stream().mapToDouble(Topping::calculateCalories).sum();
+    }
+}
