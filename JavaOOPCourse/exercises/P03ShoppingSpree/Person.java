8f1f8a4b018ac4c2a1947a2d9cda70574e41b88b
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-03 02:14:51 +0200

    Create Person.java

---
+++ JavaOOPCourse/exercises/P03ShoppingSpree/Person.java
@@ -0,0 +1,50 @@
+package S2_Encapsulation.P03ShoppingSpree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person(String name, double money) {
+        setName(name);
+        setMoney(money);
+        this.products = new ArrayList<>();
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        if (!name.trim().isEmpty()) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+    }
+
+    private void setMoney(double money) {
+        if (money >= 0) {
+            this.money = money;
+        } else {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+    }
+
+    public void buyProduct(Product product) {
+        if (this.money >= product.getCost()) {
+            this.products.add(product);
+            this.money -= product.getCost();
+        } else {
+            throw new IllegalArgumentException(String.format("%s can't afford %s", this.name, product.getName()));
+        }
+    }
+}
