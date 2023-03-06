c33af20786f07b651bb324bf8722f4a467e35cab
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-06 16:32:34 +0200

    Create Main.java

---
+++ JavaOOPCourse/labs/S3_Inheritance/P03RandomArrayList/Main.java
@@ -0,0 +1,13 @@
+package S3_Inheritance.P03RandomArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RandomArrayList randomArrayList = new RandomArrayList();
+
+        randomArrayList.getRandomElement();
+
+    }
+
+
+}
