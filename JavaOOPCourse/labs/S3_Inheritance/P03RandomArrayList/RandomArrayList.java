cf85c8b254075258579f7bbc1613afcccbba2409
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-06 16:32:47 +0200

    Create RandomArrayList.java

---
+++ JavaOOPCourse/labs/S3_Inheritance/P03RandomArrayList/RandomArrayList.java
@@ -0,0 +1,12 @@
+package S3_Inheritance.P03RandomArrayList;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList {
+
+    public Object getRandomElement() {
+        int index = new Random().nextInt(super.size());
+        return super.remove(index);
+    }
+}
