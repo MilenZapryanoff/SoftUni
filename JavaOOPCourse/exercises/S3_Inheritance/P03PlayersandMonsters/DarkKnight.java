5815904691e50c2ba2fa17a216e6d63d1d506a29
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 13:11:07 +0200

    Create DarkKnight.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P03PlayersandMonsters/DarkKnight.java
@@ -0,0 +1,7 @@
+package S3_Inheritance.P03PlayersandMonsters;
+
+public class DarkKnight extends Knight{
+    public DarkKnight(String username, int level) {
+        super(username, level);
+    }
+}
