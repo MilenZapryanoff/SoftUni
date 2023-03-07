d245b0749d3ffa509bd94f6f618931c5f0ec3791
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 14:17:05 +0200

    Create CrossMotorcycle.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P04NeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package S3_Inheritance.P04NeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle{
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
