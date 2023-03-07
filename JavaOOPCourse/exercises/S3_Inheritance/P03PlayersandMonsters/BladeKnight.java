c8f5e3c2d5cbc7478698bd64d618d592c8e29c86
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 13:10:58 +0200

    Create BladeKnight.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P03PlayersandMonsters/BladeKnight.java
@@ -0,0 +1,7 @@
+package S3_Inheritance.P03PlayersandMonsters;
+
+public class BladeKnight extends DarkKnight {
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+}
