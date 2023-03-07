86e27764ac9df6cd4ecb044785e706c0e5a8f1b0
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 16:46:10 +0200

    Create ColdBeverage.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P05Restaurant/ColdBeverage.java
@@ -0,0 +1,9 @@
+package S3_Inheritance.P05Restaurant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage{
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
