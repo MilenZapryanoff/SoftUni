f9b9a6b324e4c8061a1a441c9ff4a6fe54f772ef
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 16:47:27 +0200

    Create Tea.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P05Restaurant/Tea.java
@@ -0,0 +1,9 @@
+package S3_Inheritance.P05Restaurant;
+
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage{
+    public Tea(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
