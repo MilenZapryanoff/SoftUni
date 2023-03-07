215a01605d315e986291cad9ef635822f7df232c
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 13:12:01 +0200

    Create SoulMaster.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P03PlayersandMonsters/SoulMaster.java
@@ -0,0 +1,7 @@
+package S3_Inheritance.P03PlayersandMonsters;
+
+public class SoulMaster extends DarkWizard{
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
