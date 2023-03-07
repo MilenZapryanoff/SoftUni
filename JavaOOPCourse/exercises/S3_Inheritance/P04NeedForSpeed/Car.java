49281a00c2d36656e8cec54246f41b821586f4d2
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 14:16:56 +0200

    Create Car.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P04NeedForSpeed/Car.java
@@ -0,0 +1,10 @@
+package S3_Inheritance.P04NeedForSpeed;
+
+public class Car extends Vehicle {
+    public final static double DEFAULT_FUEL_CONSUMPTION = 3;
+
+    public Car(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
