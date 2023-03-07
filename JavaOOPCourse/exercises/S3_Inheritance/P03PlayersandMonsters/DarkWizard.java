3588bb41ac88a2d2648dfca5d860fc716323f634
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 13:11:15 +0200

    Create DarkWizard.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P03PlayersandMonsters/DarkWizard.java
@@ -0,0 +1,7 @@
+package S3_Inheritance.P03PlayersandMonsters;
+
+public class DarkWizard extends Wizard{
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
