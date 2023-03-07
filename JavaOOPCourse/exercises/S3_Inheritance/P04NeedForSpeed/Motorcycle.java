12cb44e7f6cc0f3f696a18cc154302613b720ed1
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 14:17:27 +0200

    Create Motorcycle.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P04NeedForSpeed/Motorcycle.java
@@ -0,0 +1,8 @@
+package S3_Inheritance.P04NeedForSpeed;
+
+public class Motorcycle extends Vehicle{
+
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
