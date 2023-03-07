96d915e344ef70bfcd09ba7af8339631e82ffc87
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-07 16:46:18 +0200

    Create Dessert.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P05Restaurant/Dessert.java
@@ -0,0 +1,20 @@
+package S3_Inheritance.P05Restaurant;
+
+import java.math.BigDecimal;
+
+public class Dessert extends Food {
+    private double calories;
+
+    public Dessert(String name, BigDecimal price, double grams, double calories) {
+        super(name, price, grams);
+        this.calories = calories;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+}
