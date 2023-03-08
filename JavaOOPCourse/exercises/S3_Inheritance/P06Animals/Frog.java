4c3887ae508ab023e9401126b81a47a5c536b747
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-08 20:28:15 +0200

    Create Frog.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P06Animals/Frog.java
@@ -0,0 +1,11 @@
+package S3_Inheritance.P06Animals;
+
+public class Frog extends Animal{
+    public Frog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    public String produceSound(){
+        return "Ribbit";
+    }
+}
