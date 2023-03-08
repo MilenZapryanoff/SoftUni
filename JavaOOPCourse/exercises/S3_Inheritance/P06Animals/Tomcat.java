9eec14666ed70f20ad74890270999b8fce24063d
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-08 20:28:50 +0200

    Create Tomcat.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P06Animals/Tomcat.java
@@ -0,0 +1,11 @@
+package S3_Inheritance.P06Animals;
+
+public class Tomcat extends Cat{
+    public Tomcat(String name, int age) {
+        super(name, age, "Male");
+    }
+
+    public String produceSound(){
+        return "MEOW";
+    }
+}
