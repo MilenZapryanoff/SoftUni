916a83c5bd1d9341d41f23e5afe50b0ce3512018
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-08 20:28:25 +0200

    Create Kitten.java

---
+++ JavaOOPCourse/exercises/S3_Inheritance/P06Animals/Kitten.java
@@ -0,0 +1,13 @@
+package S3_Inheritance.P06Animals;
+
+public class Kitten extends Cat{
+    public Kitten(String name, int age) {
+        super(name, age, "Female");
+    }
+
+    public String produceSound(){
+        return "Meow";
+    }
+
+
+}
