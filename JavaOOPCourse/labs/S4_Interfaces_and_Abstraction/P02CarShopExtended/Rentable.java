781dd18228ed9abe98cfa5fbb40dd12c6950f7da
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-12 20:26:21 +0200

    Update Rentable.java

--- JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P02CarShopExtended/Rentable.java
+++ JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P02CarShopExtended/Rentable.java
@@ -1,6 +1,6 @@
 package S4_Interfaces_and_Abstraction.P02CarShopExtended;
 
-public interface Rentable extends Car{
+public interface Rentable extends Car {
     Integer getMinRentDay();
 
     Double getPricePerDay();
