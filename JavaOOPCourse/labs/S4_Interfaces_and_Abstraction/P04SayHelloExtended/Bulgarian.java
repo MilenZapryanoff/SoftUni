b91a2a357dfb33600b0c44220dd8fdadb70cebbb
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-13 00:39:43 +0200

    Create Bulgarian.java

---
+++ JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P04SayHelloExtended/Bulgarian.java
@@ -0,0 +1,13 @@
+package S4_Interfaces_and_Abstraction.P04SayHelloExtended;
+
+public class Bulgarian extends BasePerson {
+
+    public Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
