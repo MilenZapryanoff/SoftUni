0057c88301ee7b2ab0906c68979649ca6303eaef
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-12 20:19:26 +0200

    Create Seat.java

---
+++ JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P02CarShopExtended/Seat.java
@@ -0,0 +1,18 @@
+package S4_Interfaces_and_Abstraction.P02CarShopExtended;
+
+public class Seat extends CarImpl implements Sellable {
+
+    private Double price;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced, Double price) {
+        super(model, color, horsePower, countryProduced);
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have %d tires%n%s sells for %f",
+                super.getModel(), super.getCountryProduced(),
+                Car.TYRES, super.getModel(), this.price);
+    }
+}
