163c3e1a1b2f61a687c977095c401918f55e27cf
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-12 20:18:48 +0200

    Create Car.java

---
+++ JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P02CarShopExtended/Car.java
@@ -0,0 +1,14 @@
+package S4_Interfaces_and_Abstraction.P02CarShopExtended;
+
+public interface Car {
+
+    public static final Integer TYRES = 4;
+
+    public String getModel();
+
+    public String getColor();
+
+    public Integer getHorsePower();
+
+    public String countryProduced();
+}
