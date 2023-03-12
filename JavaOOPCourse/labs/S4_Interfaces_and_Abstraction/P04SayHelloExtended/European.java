970d07da9f8f0295845627407a128506d1139c0d
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-13 00:39:53 +0200

    Create European.java

---
+++ JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P04SayHelloExtended/European.java
@@ -0,0 +1,13 @@
+package S4_Interfaces_and_Abstraction.P04SayHelloExtended;
+
+public class European extends BasePerson {
+
+    public European(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+}
