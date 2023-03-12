ec3600850fde32ff5127e8236b9ac9859e93e506
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-12 20:18:38 +0200

    Create Audi.java

---
+++ JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P02CarShopExtended/Audi.java
@@ -0,0 +1,29 @@
+package S4_Interfaces_and_Abstraction.P02CarShopExtended;
+
+public class Audi extends CarImpl implements Rentable {
+    private Integer minRentDay;
+    private Double pricePerDay;
+
+    public Audi(String model, String color, Integer horsePower, String countryProduced, Integer minRentDay, Double pricePerDay) {
+        super(model, color, horsePower, countryProduced);
+        this.minRentDay = minRentDay;
+        this.pricePerDay = pricePerDay;
+    }
+
+    @Override
+    public Integer getMinRentDay() {
+        return this.minRentDay;
+    }
+
+    @Override
+    public Double getPricePerDay() {
+        return this.pricePerDay;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have %d tires%nMinimum rental period of %d days. Price per day %f",
+                super.getModel(), super.getCountryProduced(),
+                Car.TYRES, this.minRentDay, this.pricePerDay);
+    }
+}
