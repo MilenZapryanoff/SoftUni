7c5b564f3d7d8bd842c5d61485811be81488c287
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-13 00:39:48 +0200

    Create Chinese.java

---
+++ JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P04SayHelloExtended/Chinese.java
@@ -0,0 +1,13 @@
+package S4_Interfaces_and_Abstraction.P04SayHelloExtended;
+
+public class Chinese extends BasePerson {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
