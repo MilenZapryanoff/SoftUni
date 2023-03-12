ee21cd46c9f6322690e7c486ca15f3b2c5fa1ae3
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-13 00:39:08 +0200

    Create Bulgarian.java

---
+++ JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P03SayHello/Bulgarian.java
@@ -0,0 +1,20 @@
+package S4_Interfaces_and_Abstraction.P03SayHello;
+
+public class Bulgarian implements Person{
+
+    private String name;
+
+    public Bulgarian(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
