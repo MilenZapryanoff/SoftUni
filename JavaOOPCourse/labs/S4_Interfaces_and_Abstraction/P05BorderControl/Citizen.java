926c55168d517edf3da20e00b4cc8906d0d3b305
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-13 23:32:51 +0200

    Create Citizen.java

---
+++ JavaOOPCourse/labs/S4_Interfaces_and_Abstraction/P05BorderControl/Citizen.java
@@ -0,0 +1,27 @@
+package S4_Interfaces_and_Abstraction.P05BorderControl;
+
+public class Citizen implements Identifiable {
+
+    private String name;
+    private int age;
+    private String id;
+
+    public Citizen(String name, int age, String id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
