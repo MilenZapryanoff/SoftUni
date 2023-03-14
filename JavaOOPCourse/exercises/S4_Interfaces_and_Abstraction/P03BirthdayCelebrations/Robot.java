bfaa8b37a33c4d9b2c43481294710c6c1481fa33
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-14 14:39:05 +0200

    Create Robot.java

---
+++ JavaOOPCourse/exercises/S4_Interfaces_and_Abstraction/P03BirthdayCelebrations/Robot.java
@@ -0,0 +1,18 @@
+package S4_Interfaces_and_Abstraction.P03BirthdayCelebrations;
+
+public class Robot implements Identifiable {
+    private String id;
+    private String model;
+
+    public Robot(String id, String model){
+        this.id = id;
+        this.model = model;
+    }
+    @Override
+    public String getId() {
+        return this.id;
+    }
+    public String getModel(){
+        return this.model;
+    }
+}
