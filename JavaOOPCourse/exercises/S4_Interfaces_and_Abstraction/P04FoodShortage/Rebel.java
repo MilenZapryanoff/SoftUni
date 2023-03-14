19d86b7efe437e6fdf531b5f4d058e0906339104
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-14 14:58:00 +0200

    Create Rebel.java

---
+++ JavaOOPCourse/exercises/S4_Interfaces_and_Abstraction/P04FoodShortage/Rebel.java
@@ -0,0 +1,38 @@
+package S4_Interfaces_and_Abstraction.P04FoodShortage;
+
+public class Rebel implements Buyer, Person {
+    private String name;
+    private int age;
+    private String group;
+    private int food = 0;
+
+    public Rebel(String name, int age, String group) {
+        this.name = name;
+        this.age = age;
+        this.group = group;
+    }
+
+    public String getGroup() {
+        return this.group;
+    }
+
+    @Override
+    public void buyFood() {
+        this.food = this.food + 5;
+    }
+
+    @Override
+    public int getFood() {
+        return this.food;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+}
