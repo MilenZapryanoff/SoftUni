8e6d35b348f66dcce002663b9490d58ab495ff41
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-19 16:56:14 +0200

    Create Rectangle.java

---
+++ JavaOOPCourse/labs/S5_Polymorphism/P02Shapes/Rectangle.java
@@ -0,0 +1,30 @@
+package S5_Polymorphism.P02Shapes;
+
+public class Rectangle extends Shape {
+
+    private Double height;
+    private Double width;
+
+    public Rectangle(Double height, Double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
+
+    @Override
+    public Double calculatePerimeter() {
+        return 2 * (this.height + this.width);
+    }
+
+    @Override
+    public Double calculateArea() {
+        return this.height * this.width;
+    }
+}
