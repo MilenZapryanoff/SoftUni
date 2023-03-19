39fc8136d37b0b77d832736da7015cbb28e547fd
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-19 16:21:44 +0200

    Create MathOperation.java

---
+++ JavaOOPCourse/labs/S5_Polymorphism/P01MathOperation/MathOperation.java
@@ -0,0 +1,15 @@
+package S5_Polymorphism.P01MathOperation;
+
+public class MathOperation {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+}
