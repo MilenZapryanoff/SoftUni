32cc95a0cc6efaf8183c93e5cfbedf93c0368064
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-03-26 13:51:26 +0300

    Create Main.java

---
+++ JavaOOPCourse/labs/S6_Reflection/P01Reflection/Main.java
@@ -0,0 +1,17 @@
+package S6_Reflection.P01Reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class reflection = Reflection.class;
+
+        System.out.println(reflection);
+        System.out.println(reflection.getSuperclass());
+        Class[] interfaces = reflection.getInterfaces();
+        Arrays.stream(interfaces).forEach(System.out::println);
+        Object reflectionObject = reflection.getDeclaredConstructor().newInstance();
+        System.out.println(reflectionObject);
+    }
+}
