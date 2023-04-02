969143ef559dc16061356d6996c58f9a46e8c789
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-04-03 01:33:15 +0300

    Create Gunner.java

---
+++ JavaOOPCourse/exercises/S6_Reflection/P03BarracksWarsANewFactory/units/Gunner.java
@@ -0,0 +1,9 @@
+package S6_Reflection.P03BarracksWarsANewFactory.units;
+
+public class Gunner extends AbstractUnit{
+    public static final int GUNNER_HEALTH = 20;
+    public static final int GUNNER_DAMAGE = 20;
+    public Gunner() {
+        super(GUNNER_HEALTH, GUNNER_DAMAGE);
+    }
+}
