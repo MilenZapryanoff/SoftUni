51920cc3a65afb0bee7ae887beaa46c17b33ad70
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-04-05 11:36:01 +0300

    Create Swordsman.java

---
+++ JavaOOPCourse/exercises/S6_Reflection/P03BarracksWarsANewFactory/units/Swordsman.java
@@ -0,0 +1,10 @@
+package S6_Reflection.P03BarracksWarsANewFactory.units;
+
+public class Swordsman extends AbstractUnit {
+    private static final int SWORDSMAN_HEALTH = 40;
+    private static final int SWORDSMAN_DAMAGE = 13;
+
+    public Swordsman() {
+        super(SWORDSMAN_HEALTH, SWORDSMAN_DAMAGE);
+    }
+}
