3b3b6c71c302dcc746f7f5973274e8f76800ee2c
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-04-14 11:32:49 +0300

    Create MainService.java

---
+++ JavaOOPCourse/exam/RobotServiceApp/src/main/java/robotService/entities/services/MainService.java
@@ -0,0 +1,9 @@
+package robotService.entities.services;
+
+public class MainService extends BaseService {
+    private static final int DEFAULT_CAPACITY = 30;
+
+    public MainService(String name) {
+        super(name, DEFAULT_CAPACITY);
+    }
+}
