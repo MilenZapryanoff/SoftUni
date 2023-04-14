1c1585af8b0d0b55c6f2bd439cd887566b0195d5
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-04-14 11:32:14 +0300

    Create BaseRobot.java

---
+++ JavaOOPCourse/exam/RobotServiceApp/src/main/java/robotService/entities/robot/BaseRobot.java
@@ -0,0 +1,65 @@
+package robotService.entities.robot;
+
+import robotService.common.ExceptionMessages;
+
+public abstract class BaseRobot implements Robot {
+    private String name;
+    private String kind;
+    private int kilograms;
+    private double price;
+
+    public BaseRobot(String name, String kind, int kilograms, double price) {
+        this.setName(name);
+        this.setKind(kind);
+        this.setKilograms(kilograms);
+        this.setPrice(price);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        if (name.trim().isEmpty() || name == null) {
+            throw new NullPointerException(ExceptionMessages.ROBOT_NAME_CANNOT_BE_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    public String getKind() {
+        return this.kind;
+    }
+
+    public void setKind(String kind) {
+        if (kind.trim().isEmpty() || kind == null) {
+            throw new NullPointerException(ExceptionMessages.ROBOT_KIND_CANNOT_BE_NULL_OR_EMPTY);
+        }
+        this.kind = kind;
+    }
+
+    @Override
+    public int getKilograms() {
+        return this.kilograms;
+    }
+
+    protected void setKilograms(int kilograms) {
+        this.kilograms = kilograms;
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException(ExceptionMessages.ROBOT_PRICE_CANNOT_BE_BELOW_OR_EQUAL_TO_ZERO);
+        }
+        this.price = price;
+    }
+
+    @Override
+    public abstract void eating();
+}
