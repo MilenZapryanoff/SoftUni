aa01eba16af4205fbd0063e8b495404d4a97c4b3
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-04-18 14:04:16 +0300

    Create PlasticArmor.java

---
+++ JavaOOPCourse/exam/RobotServiceApp/src/main/java/robotService/entities/supplements/PlasticArmor.java
@@ -0,0 +1,10 @@
+package robotService.entities.supplements;
+
+public class PlasticArmor extends BaseSupplement {
+    private static final int DEFAULT_HARDNESS = 1;
+    private static final int DEFAULT_PRICE = 10;
+
+    public PlasticArmor() {
+        super(DEFAULT_HARDNESS, DEFAULT_PRICE);
+    }
+}
