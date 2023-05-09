a9058c8c517e420d8f053069c20bdf4f730028da
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-05-09 15:42:07 +0300

    Create LongHouse.java

---
+++ JavaOOPCourse/exercises/ExamPreparation/CatHouseApp/src/main/java/catHouse/entities/houses/LongHouse.java
@@ -0,0 +1,9 @@
+package catHouse.entities.houses;
+
+public class LongHouse extends BaseHouse {
+    private static final int DEFAULT_CAPACITY = 30;
+
+    public LongHouse(String name) {
+        super(name, DEFAULT_CAPACITY);
+    }
+}
