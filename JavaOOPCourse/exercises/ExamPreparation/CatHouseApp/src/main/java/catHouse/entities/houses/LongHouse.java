e1b18551b313803f0fef88530cbbc278bb353850
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-05-09 15:41:18 +0300

    Delete JavaOOPCourse/exercises/ExamPreparation/CatHouseApp/src/main/java/catHouse/entities/houses directory

--- JavaOOPCourse/exercises/ExamPreparation/CatHouseApp/src/main/java/catHouse/entities/houses/LongHouse.java
+++
@@ -1,9 +0,0 @@
-package catHouse.entities.houses;
-
-public class LongHouse extends BaseHouse {
-    private static final int DEFAULT_CAPACITY = 30;
-
-    public LongHouse(String name) {
-        super(name, DEFAULT_CAPACITY);
-    }
-}
