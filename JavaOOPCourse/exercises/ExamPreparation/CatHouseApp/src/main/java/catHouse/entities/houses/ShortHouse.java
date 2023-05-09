9f1986299a74c596ba3815147e5d546eff1306c7
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-05-09 15:42:24 +0300

    Create ShortHouse.java

---
+++ JavaOOPCourse/exercises/ExamPreparation/CatHouseApp/src/main/java/catHouse/entities/houses/ShortHouse.java
@@ -0,0 +1,9 @@
+package catHouse.entities.houses;
+
+public class ShortHouse extends BaseHouse{
+
+    private static final int DEFAULT_CAPACITY = 15;
+    public ShortHouse(String name) {
+        super(name, DEFAULT_CAPACITY);
+    }
+}
