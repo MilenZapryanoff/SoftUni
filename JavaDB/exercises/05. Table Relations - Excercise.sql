f59fad01097642733bc8d499ef5704ed4c23ad6c
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-05-28 18:45:00 +0300

    Update 05. Table Relations - Excercise.sql

--- JavaDB/exercises/05. Table Relations - Excercise.sql
+++ JavaDB/exercises/05. Table Relations - Excercise.sql
@@ -157,3 +157,56 @@ CONSTRAINT pk_order_items PRIMARY KEY (order_id, item_id)
 );
 
 #6 - University Database 
+
+CREATE DATABASE university;
+USE university;
+
+CREATE TABLE subjects (
+    subject_id INT PRIMARY KEY AUTO_INCREMENT,
+    `subject_name` VARCHAR(50)
+);
+
+CREATE TABLE majors (
+    major_id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_number VARCHAR(12),
+    student_name VARCHAR(50),
+    major_id INT,
+    CONSTRAINT fk_students_majors FOREIGN KEY (major_id)
+        REFERENCES majors (major_id)
+);
+
+CREATE TABLE agenda (
+    student_id INT,
+    subject_id INT,
+    CONSTRAINT fk_agenda_students FOREIGN KEY (student_id)
+        REFERENCES students (student_id),
+    CONSTRAINT fk_agenda_subjects FOREIGN KEY (subject_id)
+        REFERENCES subjects (subject_id),
+    CONSTRAINT pk_agenda PRIMARY KEY (student_id , subject_id)
+);
+
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY AUTO_INCREMENT,
+    payment_date DATE,
+    payment_amount DECIMAL(8,2),
+    student_id INT,
+    CONSTRAINT fk_payments_students FOREIGN KEY (student_id)
+        REFERENCES students (student_id)
+);
+
+#9 Peaks in Rila 
+
+SELECT 
+    m.mountain_range, p.peak_name, p.elevation
+FROM
+    peaks p,
+    mountains m
+WHERE
+    p.mountain_id = m.id
+        AND m.mountain_range LIKE 'Rila%'
+ORDER BY 3 DESC;
