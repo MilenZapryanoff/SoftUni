39534e3b2565eb5dc948720fc3c4090e8ab1bda5
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-07-17 17:35:02 +0300

    Create Customer.java

---
+++ JavaDB/Spring DATA/S3_Hibernate Code First - Exercises/src/main/java/Entity/sales/Customer.java
@@ -0,0 +1,67 @@
+package Entity.sales;
+
+import Entity.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "customers")
+public class Customer extends BaseEntity {
+
+    private String name;
+    private String email;
+    private String creditCardNumber;
+    private Set<Sale> sales;
+
+    public Customer() {
+        this.sales = new HashSet<>();
+    }
+
+    public Customer(String name, String email, String creditCardNumber) {
+        this.name = name;
+        this.email = email;
+        this.creditCardNumber = creditCardNumber;
+        this.sales = new HashSet<>();
+    }
+
+    @OneToMany(mappedBy = "customer")
+    public Set<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(Set<Sale> sales) {
+        this.sales = sales;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "email", unique = true, nullable = false)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "credit_card_number")
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+}
