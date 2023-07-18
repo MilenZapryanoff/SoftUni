9916c21c5a358df24b1ae3d8c1d6316a35bdda59
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-07-18 10:41:44 +0300

    Create Course.java

---
+++ JavaDB/Spring DATA/S3_Hibernate Code First - Exercises/src/main/java/Entity/university/Course.java
@@ -0,0 +1,88 @@
+package Entity.university;
+
+import Entity.BaseEntity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "courses")
+public class Course extends BaseEntity {
+    private String name;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private int credits;
+    private Set<Student> students;
+    private Teacher teacher;
+
+    public Course() {
+        this.students = new HashSet<>();
+    }
+
+    @ManyToOne
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    @ManyToMany
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+
+    @Column(name = "name", unique = true)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "start_date")
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    @Column(name = "end_date")
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    @Column(name = "credits")
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+}
