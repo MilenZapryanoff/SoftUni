8e9941d66a5d9fa5bbca111574a0e8a5a41fa3ec
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-07-18 10:41:52 +0300

    Create Student.java

---
+++ JavaDB/Spring DATA/S3_Hibernate Code First - Exercises/src/main/java/Entity/university/Student.java
@@ -0,0 +1,49 @@
+package Entity.university;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "students")
+public class Student extends User {
+
+    private double averageGrade;
+    private Integer attendance;
+    private Set<Course> courses;
+
+    public Student() {
+        this.courses = new HashSet<>();
+    }
+
+    @ManyToMany(mappedBy = "students")
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Column(name = "average_grade")
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+
+    @Column(name = "attendance")
+    public Integer getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(Integer attendance) {
+        this.attendance = attendance;
+    }
+
+}
