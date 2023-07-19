e0b53734399365c148fb53c01f0069c2b0734a26
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-07-19 16:10:29 +0300

    Create Author.java

---
+++ JavaDB/Spring DATA/S4_Spring Data - Exercise/src/main/java/com/example/springdataexercise/model/entity/Author.java
@@ -0,0 +1,51 @@
+package com.example.springdataexercise.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "authors")
+public class Author extends BaseEntity {
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+    public Author() {
+        books = new HashSet<>();
+    }
+
+    public Author(String firstName, String lastName) {
+        this();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Column(name = "last_name", nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.EAGER)
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
