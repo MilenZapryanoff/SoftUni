0f61ab943c9fd46f87ae4e7a2b7722344046e92f
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-07-21 11:33:03 +0300

    Create SpringDataExerciseApplication.java

---
+++ JavaDB/Spring DATA/S4_Spring Data - Exercise/src/main/java/com/example/springdataexercise/SpringDataExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdataexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataExerciseApplication.class, args);
+    }
+
+}
