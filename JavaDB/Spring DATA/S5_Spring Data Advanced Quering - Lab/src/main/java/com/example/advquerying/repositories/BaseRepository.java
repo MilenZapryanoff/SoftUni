2eaaaf38c68fa2f332413a5ac2ae70a52aac9b08
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-07-27 12:19:12 +0300

    Create BaseRepository.java

---
+++ JavaDB/Spring DATA/S5_Spring Data Advanced Quering - Lab/src/main/java/com/example/advquerying/repositories/BaseRepository.java
@@ -0,0 +1,9 @@
+package com.example.advquerying.repositories;
+
+import com.example.advquerying.entities.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
+}
