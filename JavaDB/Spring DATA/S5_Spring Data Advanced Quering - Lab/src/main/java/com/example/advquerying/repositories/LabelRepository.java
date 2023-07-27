4bd0ec52a6b0f3177eca7eab6d912dc7f76639c9
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-07-27 12:19:50 +0300

    Create LabelRepository.java

---
+++ JavaDB/Spring DATA/S5_Spring Data Advanced Quering - Lab/src/main/java/com/example/advquerying/repositories/LabelRepository.java
@@ -0,0 +1,8 @@
+package com.example.advquerying.repositories;
+
+import com.example.advquerying.entities.Label;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LabelRepository extends BaseRepository<Label>{
+}
