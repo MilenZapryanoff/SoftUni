7aa7e84dc02feae09988864e84a2224072476be2
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-07-29 19:29:06 +0300

    Create SellerSeedRootDto.java

---
+++ JavaDB/Spring DATA/Exam prep 1/src/main/java/softuni/exam/models/dto/SellerSeedRootDto.java
@@ -0,0 +1,24 @@
+package softuni.exam.models.dto;
+
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@XmlRootElement(name = "sellers")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class SellerSeedRootDto {
+
+    @XmlElement(name = "seller")
+    private List<SellerSeedDto> sellers;
+
+
+    public SellerSeedRootDto() {
+    }
+
+    public List<SellerSeedDto> getSellers() {
+        return sellers;
+    }
+
+    public void setSellers(List<SellerSeedDto> sellers) {
+        this.sellers = sellers;
+    }
+}
