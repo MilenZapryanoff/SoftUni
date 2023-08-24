37c50db3f203a63f4ffb01118735da4bf7b86972
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-08-24 13:42:14 +0300

    Create AstronomerSeedRootDto.java

---
+++ JavaDB/Spring DATA/Exam/src/main/java/softuni/exam/models/dto/AstronomerSeedRootDto.java
@@ -0,0 +1,29 @@
+package softuni.exam.models.dto;
+
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement(name = "astronomers")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AstronomerSeedRootDto {
+
+    @XmlElement(name = "astronomer")
+    private List<AstronomerSeedDto> astronomers;
+
+    public AstronomerSeedRootDto() {
+        astronomers = new ArrayList<>();
+    }
+
+    public List<AstronomerSeedDto> getAstronomers() {
+        return astronomers;
+    }
+
+    public void setAstronomers(List<AstronomerSeedDto> astronomers) {
+        this.astronomers = astronomers;
+    }
+}
