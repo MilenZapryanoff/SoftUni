ca1811f9f11c3d6576346a76735f7ad99c820f77
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-08-24 13:42:19 +0300

    Create ConstellationSeedDto.java

---
+++ JavaDB/Spring DATA/Exam/src/main/java/softuni/exam/models/dto/ConstellationSeedDto.java
@@ -0,0 +1,36 @@
+package softuni.exam.models.dto;
+
+import com.google.gson.annotations.Expose;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class ConstellationSeedDto {
+
+    @Expose
+    private String name;
+    @Expose
+    private String description;
+
+    public ConstellationSeedDto() {
+    }
+    @NotNull
+    @Size(min = 3, max = 20)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @NotNull
+    @Size(min = 5)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
