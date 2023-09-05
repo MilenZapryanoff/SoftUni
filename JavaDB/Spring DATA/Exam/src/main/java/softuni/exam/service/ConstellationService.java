2070207990bd38f6e164b23eebfc58696fa5ba5e
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-09-05 12:46:22 +0300

    Create ConstellationService.java

---
+++ JavaDB/Spring DATA/Exam/src/main/java/softuni/exam/service/ConstellationService.java
@@ -0,0 +1,18 @@
+package softuni.exam.service;
+
+import softuni.exam.models.entity.Constellation;
+
+import java.io.IOException;
+
+// TODO: Implement all methods
+
+public interface ConstellationService {
+
+    boolean areImported();
+
+    String readConstellationsFromFile() throws IOException;
+
+    String importConstellations() throws IOException;
+
+    Constellation findById(Long constellation);
+}
