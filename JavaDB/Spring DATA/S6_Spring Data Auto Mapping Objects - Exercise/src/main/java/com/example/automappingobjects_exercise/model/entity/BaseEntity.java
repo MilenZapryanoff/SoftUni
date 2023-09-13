015b954c9063355ae18db5432d6b43c86dd7f202
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-09-13 10:00:44 +0300

    Create BaseEntity.java

---
+++ JavaDB/Spring DATA/S6_Spring Data Auto Mapping Objects - Exercise/src/main/java/com/example/automappingobjects_exercise/model/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.example.automappingobjects_exercise.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    private Long id;
+
+    public BaseEntity() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
+
