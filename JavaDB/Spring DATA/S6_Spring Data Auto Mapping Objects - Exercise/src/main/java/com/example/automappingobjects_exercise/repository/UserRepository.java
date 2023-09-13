0a92c0f1df4642d5d18efc9d875eca5c2085f7be
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-09-13 10:01:17 +0300

    Create UserRepository.java

---
+++ JavaDB/Spring DATA/S6_Spring Data Auto Mapping Objects - Exercise/src/main/java/com/example/automappingobjects_exercise/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.automappingobjects_exercise.repository;
+
+import com.example.automappingobjects_exercise.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
