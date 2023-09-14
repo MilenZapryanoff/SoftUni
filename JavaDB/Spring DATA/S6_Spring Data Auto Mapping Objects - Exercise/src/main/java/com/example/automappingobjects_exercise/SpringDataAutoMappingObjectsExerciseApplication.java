c734da56e7fe045c2e534e955fd8dcc1876b6070
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-09-14 11:24:36 +0300

    Create SpringDataAutoMappingObjectsExerciseApplication.java

---
+++ JavaDB/Spring DATA/S6_Spring Data Auto Mapping Objects - Exercise/src/main/java/com/example/automappingobjects_exercise/SpringDataAutoMappingObjectsExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.automappingobjects_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAutoMappingObjectsExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataAutoMappingObjectsExerciseApplication.class, args);
+    }
+
+}
