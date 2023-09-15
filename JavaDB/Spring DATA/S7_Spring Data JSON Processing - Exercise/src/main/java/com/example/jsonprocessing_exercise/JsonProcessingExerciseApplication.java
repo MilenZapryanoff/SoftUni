09a4ca32ef3cc6be95d7fcc03dd8d9794c8b6863
Milen Zapryanov <milen.zapryanov@gmail.com>
2023-09-15 09:46:07 +0300

    Create JsonProcessingExerciseApplication.java

---
+++ JavaDB/Spring DATA/S7_Spring Data JSON Processing - Exercise/src/main/java/com/example/jsonprocessing_exercise/JsonProcessingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonprocessing_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonProcessingExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonProcessingExerciseApplication.class, args);
+    }
+
+}
