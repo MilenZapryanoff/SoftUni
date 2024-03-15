b020d93d5f048ba98d92a712403979ff5f6fa66b
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-03-15 22:59:40 +0200

    Create 3.ListOfNames.js

---
+++ JS Front-End/02. JS Arrays & Strings - exercise/3.ListOfNames.js
@@ -0,0 +1,10 @@
+function solve(names) {
+    names
+        .sort((a, b) => a.localeCompare(b) )
+        .forEach((name, index) => {
+            console.log(`${index + 1}.${name}`);
+        })
+}
+
+
+solve(["John", "bob", "Christina", "Ema"]);
