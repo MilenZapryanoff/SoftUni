4f85b2b17467be69a333bee8acf5a7e3c3470e64
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-03-17 13:00:20 +0200

    Create 4.Substring.js

---
+++ JS Front-End/02. JS Arrays & Strings/4.Substring.js
@@ -0,0 +1,7 @@
+function solve (input, startPosition, numberOfSymbols) {
+    console.log(input.substring(startPosition, startPosition + numberOfSymbols));
+}
+
+
+solve ('ASentence', 1, 8);
+solve ('SkipWord', 4, 7);
