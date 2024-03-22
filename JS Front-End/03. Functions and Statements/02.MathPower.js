1465a4fd2da3e3626cf58d8c83f6c81cbad383f2
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-03-22 16:21:32 +0200

    Create 02.MathPower.js

---
+++ JS Front-End/03. Functions and Statements/02.MathPower.js
@@ -0,0 +1,11 @@
+function solve(number, power) {
+
+    let result = 1
+    for (let i = 0; i < power; i++) {
+        result = result * number;
+    }
+    console.log(result)
+}
+
+solve(2, 8);
+solve(3, 4);
