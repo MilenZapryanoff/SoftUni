94cadaefc6e2a52e1ed759195ad2144e29a7dccf
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-03-26 09:42:00 +0200

    Create 07.AddressBook.js

---
+++ JS Front-End/04. Objects and Classes/07.AddressBook.js
@@ -0,0 +1,20 @@
+function solve(input) {
+    const addressBook = {};
+
+    input.forEach(line => {
+        const [name, address] = line.split(':');
+
+        addressBook[name] = address;
+    });
+
+    Object
+        .entries(addressBook)
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .forEach(([name, address]) => console.log(`${name} -> ${address}`))
+}
+
+solve(['Tim:Doe Crossing',
+    'Bill:Nelson Place',
+    'Peter:Carlyle Ave',
+    'Bill:Ornery Rd']
+)
