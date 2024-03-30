8385c40df8b79b06fc8b973173fba454810068a5
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-03-30 15:59:08 +0200

    Create 03.StoreProvision.js

---
+++ JS Front-End/04. Objects and Classes - exercise/03.StoreProvision.js
@@ -0,0 +1,31 @@
+function solve(stock, delivery) {
+    let products = {};
+
+    for (let i = 0; i < stock.length; i += 2) {
+        products[stock[i]] = stock[i + 1];
+    }
+
+    for (let i = 0; i < delivery.length; i += 2) {
+        let product = delivery[i];
+        let quantity = delivery[i + 1]
+
+        products[product] = products[product] ? Number.parseInt(products[product]) + Number.parseInt(quantity) : delivery[i + 1];
+    }
+
+    for (const product in products) {
+        console.log(`${product} -> ${products[product]}`)
+    }
+}
+
+
+solve(['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'],
+    ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30']
+);
+
+solve ([
+        'Salt', '2', 'Fanta', '4', 'Apple', '14', 'Water', '4', 'Juice', '5'
+    ],
+    [
+        'Sugar', '44', 'Oil', '12', 'Apple', '7', 'Tomatoes', '7', 'Bananas', '30'
+    ]
+);
