b353f50c1226b3212f3e15964f3669a746c2dce3
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-04-09 12:45:01 +0300

    Create report.js

---
+++ JS Front-End/05. DOM Introduction - exercise/08. Generate Report/report.js
@@ -0,0 +1,36 @@
+function generateReport() {
+    const thElements = document.querySelectorAll('table thead th');
+    const trElements = document.querySelectorAll('table tbody tr');
+    const outputElement = document.getElementById('output');
+
+    const columns = Array
+        .from(thElements)
+        .map(thElement => {
+            const checkboxElement = thElement.querySelector('input[type=checkbox]');
+
+            return {
+                name: thElement.textContent.toLowerCase().trim(),
+                active: checkboxElement.checked
+            };
+        });
+
+    const reportData = Array
+        .from(trElements)
+        .map(trElement => {
+            const tdElements = trElement.querySelectorAll('td');
+
+            const result = Array
+                .from(tdElements)
+                .filter((tdElement, i) => columns[i].active)
+                .reduce((data, tdElement, i) => {
+                    const columnName = columns[i].name;
+                    data[columnName] = tdElement.textContent;
+
+                    return data;
+                }, {})
+
+            return result;
+        })
+
+    outputElement.value = JSON.stringify(reportData, null, 2);
+}
