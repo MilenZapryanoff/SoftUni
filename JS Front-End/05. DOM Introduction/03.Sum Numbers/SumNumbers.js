e50870e6004300db9262673d9d817c80f132ae3d
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-04-11 14:51:14 +0300

    Create SumNumbers.js

---
+++ JS Front-End/05. DOM Introduction/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,7 @@
+function calc() {
+
+    let numOneElement = Number(document.getElementById('num1').value);
+    let numTwoElement = Number(document.getElementById('num2').value);
+
+    document.getElementById('sum').value = numOneElement + numTwoElement
+}
