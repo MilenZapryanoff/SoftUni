856f76a64cc5b0db9400b812abbdc49fa27cf6df
Milen Zapryanov <milen.zapryanov@gmail.com>
2024-04-11 14:51:31 +0300

    Create ShowMore.js

---
+++ JS Front-End/05. DOM Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let readMoreElement = document.getElementById('more');
+    let expandTextElement = document.getElementById('text');
+
+    readMoreElement.style.display = 'none';
+    expandTextElement.style.display = 'inline';
+}
